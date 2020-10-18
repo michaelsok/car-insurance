45c5aa1159ceb72b99208d681f5551d3863664c1
Michael SOK <mdb.sok@gmail.com>
2020-10-18 22:34:47 +0200

    feat: add model training

---
+++ carinsurance/application/train_model.py
@@ -0,0 +1,42 @@
+import os
+import pickle
+
+import pandas as pd
+
+from carinsurance import logger
+from carinsurance.config import CONFIG
+from carinsurance.domain.modelling.model import Model
+
+
+def train_model(config, logger):
+    clean_path = os.path.join(config['project'], config['data'], 'clean')
+    models_path = os.path.join(config['project'], config['models'])
+    results_path = os.path.join(config['project'], config['results'])
+
+    if not os.path.exists(results_path):
+        logger.info('Creating results_path directory...')
+        os.makedirs(results_path, exist_ok=True)
+
+    logger.info('Reading train dataset...')
+    X_train = pd.read_csv(os.path.join(clean_path, 'train.csv'))
+    y_train = pd.read_csv(os.path.join(clean_path, 'y_train.csv'))
+
+    logger.info('Reading validation dataset...')
+    X_validation = pd.read_csv(os.path.join(clean_path, 'validation.csv'))
+    y_validation = pd.read_csv(os.path.join(clean_path, 'y_validation.csv'))
+
+    logger.info('Creating model...')
+    model = Model(name='RandomForest', results_path=results_path, logger=logger)
+    logger.info('Training model...')
+    model.train(X_train, y_train.values.reshape(-1))
+    logger.info('Save report on validation...')
+    model.compute_report_using(X_validation, y_validation.values.reshape(-1), threshold=.5)
+
+    logger.info('Saving model...')
+    model.save(os.path.join(models_path))
+
+    logger.info('Model trained!')
+
+
+if __name__ == '__main__':
+    train_model(CONFIG, logger)
---
+++ carinsurance/domain/modelling/model.py
@@ -0,0 +1,52 @@
+import os
+import pickle
+import logging
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import classification_report
+
+
+MODELS = {
+    'RandomForest': RandomForestClassifier,
+    'LogisticRegression': LogisticRegression,
+    'DecisionTree': DecisionTreeClassifier,
+    'GradientBoosting': GradientBoostingClassifier,
+}
+
+
+class Model(object):
+    def __init__(self, name='RandomForest', results_path=None, logger=None, **modelargs):
+        assert name in MODELS.keys()
+        self.name = name
+        self.results_path = results_path
+        self.logger = logging.getLogger()
+        self.model = MODELS[name](**modelargs)
+
+    def train(self, features, target, **fitargs):
+        self.model.fit(features, target, **fitargs)
+
+    def predict(self, features, **predictargs):
+        if hasattr(self.model, 'predict_proba'):
+            return self.model.predict_proba(features, **predictargs)
+        return self.model.predict(features, **predictargs)
+
+    def compute_report_using(self, features, target, threshold=.5):
+        predictions = (self.predict(features)[:, 1] > threshold).astype(int)
+        report = classification_report(target, predictions)
+
+        self.logger.info(report)
+        if self.results_path is not None:
+            with open(os.path.join(self.results_path, 'report.txt'), 'w') as f:
+                f.write(report)
+
+    def save(self, models_path, name=None):
+        model_name = name or 'model.pkl'
+
+        with open(os.path.join(models_path, model_name), 'wb') as f:
+            pickle.dump(self.model, f)
+
+    def load(self, model):
+        self.model = model
+        self.name = model.__name__.replace('Classifier', '')
